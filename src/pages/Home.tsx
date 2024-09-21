1327d49a90eed2e89126e1137e9f04858a781349
Gabriel <gabriels.v9@gmail.com>
2024-09-21 20:35:16 -0300

    Feat: Start of Front of Mirror Scan

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './Home.css'
+import '../assets/global.css'
+
+export const Home:React.FC = () => {
+  return (
+    <div className='home-container'>
+      <h1>Home</h1>
+    </div>
+  )
+}
